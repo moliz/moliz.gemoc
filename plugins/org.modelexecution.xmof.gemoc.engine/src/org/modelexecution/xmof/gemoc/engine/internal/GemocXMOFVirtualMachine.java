fc6722631dfdcd0505521969f8d8b9227c5822e6
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2016-04-30 21:37:53 +0200

    Sequentializes xMOF execution

--- plugins/org.modelexecution.xmof.gemoc.engine/src/org/modelexecution/xmof/gemoc/engine/internal/GemocXMOFVirtualMachine.java
+++ plugins/org.modelexecution.xmof.gemoc.engine/src/org/modelexecution/xmof/gemoc/engine/internal/GemocXMOFVirtualMachine.java
@@ -1,8 +1,13 @@
 package org.modelexecution.xmof.gemoc.engine.internal;
 
+import java.util.Collection;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.modelexecution.fumldebug.core.event.ActivityEntryEvent;
+import org.modelexecution.fumldebug.core.event.Event;
 import org.modelexecution.xmof.vm.XMOFVirtualMachine;
+import org.modelexecution.xmof.vm.XMOFVirtualMachineEvent;
 
 public class GemocXMOFVirtualMachine extends XMOFVirtualMachine {
 
@@ -25,6 +30,23 @@ public class GemocXMOFVirtualMachine extends XMOFVirtualMachine {
 				.getInstanceMap()).getBuilder();
 		EClass xmofEClass = builder.getXMOFConfigurationClass(registeredEClass);
 		return xmofEClass;
+	}
+
+	@Override
+	protected Collection<XMOFVirtualMachineEvent> processRawEvent(Event event) {
+		Collection<XMOFVirtualMachineEvent> eventsToDeliver = super
+				.processRawEvent(event);
+		if (event instanceof ActivityEntryEvent) {
+			ActivityEntryEvent activityEntryEvent = (ActivityEntryEvent) event;
+			int activityExecutionID = activityEntryEvent.getActivityExecutionID();
+			setActivityExecutionID(activityExecutionID);
+		}
+		return eventsToDeliver;
+	}
+
+	@Override
+	protected void resumeExecution() {
+		getRawExecutionContext().nextStep(getActivityExecutionID());
 	}
 
 }
