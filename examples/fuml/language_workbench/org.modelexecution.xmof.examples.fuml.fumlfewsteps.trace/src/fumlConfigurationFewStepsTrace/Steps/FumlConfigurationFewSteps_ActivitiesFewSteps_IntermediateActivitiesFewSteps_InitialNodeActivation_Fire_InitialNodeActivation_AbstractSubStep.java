32b1beec1c41749c46302caebe2acc97e8985a59
Erwan Bousse <ebousse@users.noreply.github.com>
2016-06-17 04:40:15 +0200

    Add missing files for partial trace addon

---
+++ examples/fuml/language_workbench/org.modelexecution.xmof.examples.fuml.fumlfewsteps.trace/src/fumlConfigurationFewStepsTrace/Steps/FumlConfigurationFewSteps_ActivitiesFewSteps_IntermediateActivitiesFewSteps_InitialNodeActivation_Fire_InitialNodeActivation_AbstractSubStep.java
@@ -0,0 +1,17 @@
+/**
+ */
+package fumlConfigurationFewStepsTrace.Steps;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Fuml Configuration Few Steps Activities Few Steps Intermediate Activities Few Steps Initial Node Activation Fire Initial Node Activation Abstract Sub Step</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see fumlConfigurationFewStepsTrace.Steps.StepsPackage#getFumlConfigurationFewSteps_ActivitiesFewSteps_IntermediateActivitiesFewSteps_InitialNodeActivation_Fire_InitialNodeActivation_AbstractSubStep()
+ * @model interface="true" abstract="true"
+ * @generated
+ */
+public interface FumlConfigurationFewSteps_ActivitiesFewSteps_IntermediateActivitiesFewSteps_InitialNodeActivation_Fire_InitialNodeActivation_AbstractSubStep extends SpecificStep {
+} // FumlConfigurationFewSteps_ActivitiesFewSteps_IntermediateActivitiesFewSteps_InitialNodeActivation_Fire_InitialNodeActivation_AbstractSubStep
