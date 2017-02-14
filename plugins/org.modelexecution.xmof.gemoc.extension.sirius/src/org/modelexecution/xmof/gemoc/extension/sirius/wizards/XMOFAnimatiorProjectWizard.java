6ed9331bae1128c5c5bd86d2427a17975c764c33
Tobias Ortmayr <tobmayr@hotmail.com>
2017-02-14 02:53:31 +0100

    Moves code for xMOF animator into seperate plugin. Naming scheme anlog to corresponding GEMOC plugin

---
+++ plugins/org.modelexecution.xmof.gemoc.extension.sirius/src/org/modelexecution/xmof/gemoc/extension/sirius/wizards/XMOFAnimatiorProjectWizard.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2017 Vienna University of Technology.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 which accompanies 
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Tobias Ortmayr - initial API and implementation
+ */
+
+package org.modelexecution.xmof.gemoc.extension.sirius.wizards;
+
+import org.gemoc.xdsmlframework.extensions.sirius.wizards.NewGemocDebugRepresentationWizard;
+import org.gemoc.xdsmlframework.extensions.sirius.wizards.pages.AddDebugRepresentationPage;
+import org.gemoc.xdsmlframework.extensions.sirius.wizards.pages.DebugRepresentationSelectionPage;
+import org.gemoc.xdsmlframework.extensions.sirius.wizards.pages.NewViewPointProjectPage;
+import org.gemoc.xdsmlframework.extensions.sirius.wizards.pages.SelectDiagramDefinitionPage;
+
+public class XMOFAnimatiorProjectWizard extends NewGemocDebugRepresentationWizard {
+
+
+
+  public XMOFAnimatiorProjectWizard() {
+    super();
+    setWindowTitle("Create XMOF Sirius Animation Project");
+  }
+
+ 
+}
