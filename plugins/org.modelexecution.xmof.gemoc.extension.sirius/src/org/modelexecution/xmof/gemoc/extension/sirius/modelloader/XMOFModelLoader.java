798ff618529ae2d171578687446a99cd16d6aa6a
Tobias Ortmayr <tobias.ortmayr@gmail.com>
2017-02-23 22:14:01 +0100

    Removes implementation for opening SiriusSession form XMOFModeLoader

--- plugins/org.modelexecution.xmof.gemoc.extension.sirius/src/org/modelexecution/xmof/gemoc/extension/sirius/modelloader/XMOFModelLoader.java
+++ plugins/org.modelexecution.xmof.gemoc.extension.sirius/src/org/modelexecution/xmof/gemoc/extension/sirius/modelloader/XMOFModelLoader.java
@@ -12,36 +12,22 @@ package org.modelexecution.xmof.gemoc.extension.sirius.modelloader;
 
 import java.io.IOException;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.sirius.business.api.session.Session;
-import org.eclipse.sirius.business.api.session.SessionManager;
-import org.eclipse.sirius.business.internal.session.SessionTransientAttachment;
-import org.eclipse.sirius.ui.business.api.dialect.DialectEditor;
-import org.eclipse.sirius.ui.business.api.session.IEditingSession;
-import org.eclipse.sirius.ui.business.api.session.SessionUIManager;
-import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.PlatformUI;
-import org.gemoc.executionframework.extensions.sirius.debug.DebugSessionFactory;
-import org.gemoc.executionframework.extensions.sirius.modelloader.DebugPermissionProvider;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.gemoc.xdsmlframework.api.core.IExecutionContext;
 import org.gemoc.xdsmlframework.api.core.IModelLoader;
 
-@SuppressWarnings("restriction")
 public class XMOFModelLoader implements IModelLoader {
-	private IProgressMonitor progressMonitor;
 
 	@Override
 	public Resource loadModel(IExecutionContext context) {
 		Resource resource = null;
 		ResourceSet resourceSet;
 		resourceSet = new ResourceSetImpl();
+		TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain(resourceSet);
 		resource = resourceSet.createResource(context.getRunConfiguration().getExecutedModelURI());
 		try {
 			resource.load(null);
@@ -53,79 +39,11 @@ public class XMOFModelLoader implements IModelLoader {
 
 	@Override
 	public Resource loadModelForAnimation(IExecutionContext context) throws RuntimeException {
-		Resource modelResource = loadModel(context);
-		if (context.getRunConfiguration().getAnimatorURI() == null)
-			return modelResource;
-
-		killPreviousSiriusSession(context.getRunConfiguration().getAnimatorURI());
-		try {
-			openNewSiriusSession(context, context.getRunConfiguration().getAnimatorURI(), modelResource);
-		} catch (CoreException e) {
-			throw new RuntimeException(e);
-		}
-		return modelResource;
-
-	}
-
-	private Session openNewSiriusSession(IExecutionContext context, URI animatorURI, Resource modelResource)
-			throws CoreException {
-		SubMonitor subMonitor = SubMonitor.convert(this.progressMonitor, 5);
-		subMonitor.newChild(1);
-		final Session session = DebugSessionFactory.INSTANCE.createSession(modelResource.getResourceSet(), animatorURI);
-		if (modelResource.getContents().size() > 0) {
-			Resource res = modelResource.getContents().get(0).eResource(); // get
-																			// the
-																			// used
-			// resource
-			res.eAdapters().add(new SessionTransientAttachment(session));
-		}
-		subMonitor.subTask("Opening Sirius session");
-		session.open(subMonitor.newChild(2));
-
-		return session;
-	}
-
-	// TODO: Make method in superclass protected to avoid duplicated code
-	private void killPreviousSiriusSession(URI sessionResourceURI) {
-		final Session session = SessionManager.INSTANCE.getExistingSession(sessionResourceURI);
-		if (session != null) {
-			final IEditingSession uiSession = SessionUIManager.INSTANCE.getUISession(session);
-			DebugPermissionProvider permProvider = new DebugPermissionProvider();
-			if (!permProvider.provides(session.getTransactionalEditingDomain().getResourceSet())) {
-				// this is a not debugSession (ie. a normal editing session)
-				if (uiSession != null) {
-					for (final DialectEditor editor : uiSession.getEditors()) {
-						final IEditorSite editorSite = editor.getEditorSite();
-						if (editor.getSite() == null) {
-							editorSite.getShell().getDisplay().syncExec(new Runnable() {
-								@Override
-								public void run() {
-									editorSite.getPage().closeEditor(editor, true);
-								}
-							});
-						}
-					}
-					PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
-						@Override
-						public void run() {
-							uiSession.close();
-						}
-					});
-				}
-			}
-			PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
-				@Override
-				public void run() {
-					session.close(new NullProgressMonitor());
-					SessionManager.INSTANCE.remove(session);
-				}
-			});
-		}
+		return loadModel(context);
 	}
 
 	@Override
 	public void setProgressMonitor(IProgressMonitor progressMonitor) {
-		this.progressMonitor = progressMonitor;
 
 	}
 
