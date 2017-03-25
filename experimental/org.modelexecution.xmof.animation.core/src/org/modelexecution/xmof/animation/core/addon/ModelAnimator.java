3acd5e09c130ee886f6d8d21080b7a02145ee655
Tobias Ortmayr <tobias.ortmayr@gmail.com>
2017-03-26 00:42:24 +0100

    Moves xmofanimation plugin in "experimental" directory

---
+++ experimental/org.modelexecution.xmof.animation.core/src/org/modelexecution/xmof/animation/core/addon/ModelAnimator.java
@@ -0,0 +1,197 @@
+/*******************************************************************************
+ * Copyright (c) 2016
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.modelexecution.xmof.animation.core.addon;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.gemoc.xdsmlframework.api.core.EngineStatus.RunStatus;
+import org.gemoc.xdsmlframework.api.core.IExecutionEngine;
+import org.gemoc.xdsmlframework.api.engine_addon.IEngineAddon;
+import org.modelexecution.xmof.animation.core.controller.AnimationController;
+import org.modelexecution.xmof.animation.core.provider.AnimationProviderRegistry;
+import org.modelexecution.xmof.animation.core.provider.IAnimationProvider;
+import org.modelexecution.xmof.gemoc.engine.XMOFExecutionEngine;
+import org.modelexecution.xmof.vm.XMOFBasedModel;
+
+import fr.inria.diverse.trace.commons.model.trace.Step;
+
+/**
+ * ModelAnimator class which implements the AddonInterface for notifications of
+ * debug events.
+ * 
+ * @author Matthias Hoellthaler (e1025709@student.tuwien.ac.at)
+ * @author Tobias Ortmayr (e1026279@student.tuwien.ac.at)
+ * @version 1.0
+ *
+ */
+public class ModelAnimator implements IEngineAddon {
+
+	private AnimationController animationController;
+
+	@Override
+	public void engineAboutToStart(IExecutionEngine executionEngine) {
+
+	}
+
+	/**
+	 * After successfully starting the engine, the the animation plugin will be
+	 * initialized.s
+	 * 
+	 * @param executionEngine
+	 */
+	@Override
+	public void engineStarted(IExecutionEngine executionEngine) {
+		if (executionEngine instanceof XMOFExecutionEngine) {
+			initialize((XMOFExecutionEngine) executionEngine);
+
+		}
+
+	}
+
+	private void initialize(XMOFExecutionEngine engine) {
+		PlatformUI.getWorkbench().getDisplay().syncExec((new Runnable() {
+			@Override
+			public void run() {
+
+				animationController = retrieveController(engine);
+			}
+		}));
+
+	}
+
+	private AnimationController retrieveController(XMOFExecutionEngine engine) {
+		URI xmofModelURI = retrieveXMOFModelURI(engine);
+		XMOFBasedModel model = engine.getRawVirtualMachine().getModel();
+		AnimationProviderRegistry registry = AnimationProviderRegistry.getInstance();
+		if (registry.haveProvider(xmofModelURI)) {
+			List<IAnimationProvider> possibleProviders = registry.getProviders(xmofModelURI);
+			IAnimationProvider provider = null;
+			if (possibleProviders.size() > 1) {
+				provider = letUserSelectProvider(possibleProviders);
+			}
+			if (provider == null) {
+
+				provider = possibleProviders.get(0);
+			}
+			return provider.createController(xmofModelURI, model);
+		}
+		return null;
+	}
+
+	private URI retrieveXMOFModelURI(XMOFExecutionEngine engine) {
+		Resource modelResource = engine.getModelLoader().getXMOFModelResource();
+		return modelResource.getURI();
+	}
+
+	private IAnimationProvider letUserSelectProvider(List<IAnimationProvider> possibleProviders) {
+		Map<String, IAnimationProvider> providerMap = createProviderMap(possibleProviders);
+		ElementListSelectionDialog dialog = createSelectionDialog(providerMap.keySet().toArray());
+		// User pressed cancel
+		if (dialog.open() != Window.OK) {
+			return null;
+		}
+		Object[] result = dialog.getResult();
+
+		return providerMap.get(result[0]);
+
+	}
+
+	private ElementListSelectionDialog createSelectionDialog(Object[] elements) {
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		ElementListSelectionDialog dialog = new ElementListSelectionDialog(shell, new LabelProvider());
+		dialog.setMultipleSelection(false);
+		dialog.setElements(elements);
+		dialog.setTitle("Choose desired animation provider");
+		return dialog;
+	}
+
+	private Map<String, IAnimationProvider> createProviderMap(List<IAnimationProvider> possibleProviders) {
+		Map<String, IAnimationProvider> map = new HashMap<>();
+		for (IAnimationProvider provider : possibleProviders) {
+			map.put(provider.getName(), provider);
+		}
+		return map;
+	}
+
+	@Override
+	public void engineAboutToStop(IExecutionEngine engine) {
+		if (animationController != null) {
+			animationController.dispose();
+			animationController = null;
+		}
+	}
+
+	@Override
+	public void engineStopped(IExecutionEngine engine) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void engineAboutToDispose(IExecutionEngine engine) {
+
+	}
+
+	@Override
+	public void aboutToSelectStep(IExecutionEngine engine, Collection<Step> steps) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void proposedStepsChanged(IExecutionEngine engine, Collection<Step> steps) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stepSelected(IExecutionEngine engine, Step selectedStep) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * MSE occurrence is passed to controller to animate step
+	 */
+	@Override
+	public void aboutToExecuteStep(IExecutionEngine engine, Step stepToExecute) {
+		if (animationController != null) {
+			animationController.processMSE(stepToExecute.getMseoccurrence(), true);
+		}
+
+	}
+
+	@Override
+	public void stepExecuted(IExecutionEngine engine, Step stepExecuted) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void engineStatusChanged(IExecutionEngine engine, RunStatus newStatus) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<String> validate(List<IEngineAddon> otherAddons) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ experimental/org.modelexecution.xmof.animation.sirius/src/org/modelexecution/xmof/animation/sirius/handler/SiriusDiagramHandler.java
@@ -0,0 +1,180 @@
+/*******************************************************************************
+ * Copyright (c) 2016
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.modelexecution.xmof.animation.sirius.handler;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.sirius.business.api.dialect.command.RefreshRepresentationsCommand;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.ui.business.api.dialect.DialectEditor;
+import org.eclipse.sirius.ui.business.api.dialect.DialectUIManager;
+import org.eclipse.sirius.ui.business.api.session.IEditingSession;
+import org.eclipse.sirius.ui.business.api.session.SessionUIManager;
+import org.eclipse.sirius.viewpoint.DAnalysis;
+import org.eclipse.sirius.viewpoint.DRepresentation;
+import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
+import org.eclipse.sirius.viewpoint.DView;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.gemoc.executionframework.engine.core.CommandExecution;
+import org.gemoc.executionframework.extensions.sirius.modelloader.DebugPermissionProvider;
+import org.modelexecution.xmof.Syntax.Activities.IntermediateActivities.Activity;
+import org.modelexecution.xmof.animation.core.handler.DiagramHandler;
+
+/**
+ * Provider for Sirius animation service
+ * 
+ * @author Matthias Hoellthaler (e1025709@student.tuwien.ac.at)
+ * @author Tobias Ortmayr (e1026279@student.tuwien.ac.at)
+ * @version 1.0
+ *
+ */
+public class SiriusDiagramHandler extends DiagramHandler {
+
+	private Map<String, IEditorPart> diagramEditorMap = new HashMap<>();
+	private static String SIRIUS_SPECIFICATION_FILE = "representations.aird";
+	private URI airdURI;
+
+	public SiriusDiagramHandler(URI modelURI) {
+		super(modelURI);
+		this.airdURI = createURI();
+		killPreviousSiriusSession(airdURI);
+
+	}
+
+	private URI createURI() {
+		URI siriusURI = URI.createURI("platform:/" + xmofModelURI.segment(0) + "/" + xmofModelURI.segment(1) + "/"
+				+ SIRIUS_SPECIFICATION_FILE);
+		return siriusURI;
+	}
+
+	/**
+	 * A previous session needs to be killed before the animation can take place
+	 * 
+	 * @param sessionResourceURI
+	 *            URI of session
+	 */
+	private void killPreviousSiriusSession(URI sessionResourceURI) {
+		final Session session = SessionManager.INSTANCE.getExistingSession(sessionResourceURI);
+		if (session != null) {
+			final IEditingSession uiSession = SessionUIManager.INSTANCE.getUISession(session);
+
+			DebugPermissionProvider permProvider = new DebugPermissionProvider();
+			if (!permProvider.provides(session.getTransactionalEditingDomain().getResourceSet())) {
+				// this is a not debugSession (ie. a normal editing session)
+				if (uiSession != null) {
+					for (final DialectEditor editor : uiSession.getEditors()) {
+						final IEditorSite editorSite = editor.getEditorSite();
+						if (editor.getSite() == null) {
+							editorSite.getShell().getDisplay().syncExec(new Runnable() {
+								@Override
+								public void run() {
+									editorSite.getPage().closeEditor(editor, true);
+								}
+							});
+						}
+
+					}
+					PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+
+						@Override
+						public void run() {
+							uiSession.close();
+						}
+					});
+				}
+			}
+			PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+
+				@Override
+				public void run() {
+					session.close(new NullProgressMonitor());
+					SessionManager.INSTANCE.remove(session);
+				}
+			});
+		}
+	}
+
+	@Override
+	public void openOrShowDiagram(Activity activity) {
+		String key = activity.getName().toUpperCase();
+		if (diagramEditorMap.containsKey(key)) {
+			activateDiagramEditor(key);
+		} else {
+			openDiagramEditor(key);
+		}
+	}
+
+	public void refreshDiagram(String diagramKey) {
+		String key = diagramKey.trim().toUpperCase();
+		final Session session = SessionManager.INSTANCE.getExistingSession(airdURI);
+		if (session == null || !diagramEditorMap.containsKey(key))
+			return;
+		DialectEditor editor = (DialectEditor) diagramEditorMap.get(key);
+
+		Collection<DRepresentation> representations = Arrays.asList(editor.getRepresentation());
+		TransactionalEditingDomain editingDomain = session.getTransactionalEditingDomain();
+
+		final RefreshRepresentationsCommand refresh = new RefreshRepresentationsCommand(editingDomain,
+				new NullProgressMonitor(), representations);
+		CommandExecution.execute(editingDomain, refresh);
+
+	}
+
+	private void openDiagramEditor(String key) {
+		Session siriusSession = SessionManager.INSTANCE.getExistingSession(airdURI);
+		if (siriusSession == null) {
+			siriusSession = SessionManager.INSTANCE.getSession(airdURI, new NullProgressMonitor());
+		}
+		DAnalysis root = (DAnalysis) siriusSession.getSessionResource().getContents().get(0);
+		DView dView = root.getOwnedViews().get(0);
+		for (DRepresentationDescriptor representationDescriptor : dView.getOwnedRepresentationDescriptors()) {
+			DRepresentation representation=representationDescriptor.getRepresentation();
+			if (representation.getName().toUpperCase().contains(key)) {
+				IEditorPart editor = DialectUIManager.INSTANCE.openEditor(siriusSession, representation,
+						new NullProgressMonitor());
+				diagramEditorMap.put(key, editor);
+				return;
+			}
+		}
+
+	}
+
+	private void activateDiagramEditor(String key) {
+		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+				IWorkbenchPage page = window.getActivePage();
+				IEditorPart editor = diagramEditorMap.get(key);
+				page.activate(editor);
+
+			}
+		});
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
