ad5fa21ea25cb676ca77d15582412a056c80fbaf
Tobias Ortmayr <tobias.ortmayr@gmail.com>
2017-05-23 00:23:49 +0200

    Updates dependencies to GEMOC plugins after latest refactorings + deletes obsolote XMOFMutableFieldExtractor

--- plugins/org.modelexecution.xmof.gemoc.engine/src/org/modelexecution/xmof/gemoc/engine/ui/debug/XMOFSequentialModelDebugger.java
+++ plugins/org.modelexecution.xmof.gemoc.engine/src/org/modelexecution/xmof/gemoc/engine/ui/debug/XMOFSequentialModelDebugger.java
@@ -13,7 +13,7 @@ import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.gemoc.execution.sequential.javaengine.ui.debug.GenericSequentialModelDebugger;
-import org.gemoc.executionframework.engine.ui.debug.breakpoint.GemocBreakpoint;
+import org.gemoc.executionframework.debugger.GemocBreakpoint;
 import org.gemoc.xdsmlframework.api.core.IExecutionEngine;
 
 import fr.inria.diverse.melange.resource.MelangeResource;
@@ -22,49 +22,48 @@ import fr.obeo.dsl.debug.ide.event.IDSLDebugEventProcessor;
 
 public class XMOFSequentialModelDebugger extends GenericSequentialModelDebugger {
 
-  private MelangeResource melangeResource = null;
+	private MelangeResource melangeResource = null;
 
-  public XMOFSequentialModelDebugger(IDSLDebugEventProcessor target, IExecutionEngine engine) {
-    super(target, engine);
-    if (engine.getExecutionContext().getResourceModel() instanceof MelangeResource) {
-      melangeResource = (MelangeResource) engine.getExecutionContext().getResourceModel();
-    }
-  }
+	public XMOFSequentialModelDebugger(IDSLDebugEventProcessor target, IExecutionEngine engine) {
+		super(target, engine);
+		if (engine.getExecutionContext().getResourceModel() instanceof MelangeResource) {
+			melangeResource = (MelangeResource) engine.getExecutionContext().getResourceModel();
+		}
+	}
 
-  @Override
-  public boolean shouldBreak(EObject instruction) {
-    if (super.shouldBreak(instruction)) {
-      return true;
-    }
-    if (instruction instanceof MSEOccurrence) {
-      EObject caller = ((MSEOccurrence) instruction).getMse().getCaller();
-      if (hasRegularBreakpointTrue(caller)) {
-        return true;
-      }
-    }
+	@Override
+	public boolean shouldBreak(EObject instruction) {
+		if (super.shouldBreak(instruction)) {
+			return true;
+		}
+		if (instruction instanceof MSEOccurrence) {
+			EObject caller = ((MSEOccurrence) instruction).getMse().getCaller();
+			if (hasRegularBreakpointTrue(caller)) {
+				return true;
+			}
+		}
 
-    return false;
+		return false;
 
-  }
+	}
 
-  private boolean hasRegularBreakpointTrue(EObject o) {
-    EObject target = o;
-    Resource res = o.eResource();
-    if (melangeResource != null) {
-      target = melangeResource.getWrappedResource().getEObject(res.getURIFragment(o));
-    }
+	private boolean hasRegularBreakpointTrue(EObject o) {
+		EObject target = o;
+		Resource res = o.eResource();
+		if (melangeResource != null) {
+			target = melangeResource.getWrappedResource().getEObject(res.getURIFragment(o));
+		}
 
-    return shouldBreakOnCaller(target) && (Boolean
-        .valueOf((String) getBreakpointAttributes(target, GemocBreakpoint.BREAK_ON_LOGICAL_STEP))
-        || Boolean.valueOf(
-            (String) getBreakpointAttributes(target, GemocBreakpoint.BREAK_ON_MSE_OCCURRENCE)));
-  }
+		return shouldBreakOnCaller(target) && (Boolean
+				.valueOf((String) getBreakpointAttributes(target, GemocBreakpoint.BREAK_ON_LOGICAL_STEP))
+				|| Boolean.valueOf((String) getBreakpointAttributes(target, GemocBreakpoint.BREAK_ON_MSE_OCCURRENCE)));
+	}
 
-  public boolean shouldBreakOnCaller(EObject instruction) {
-    final boolean res;
+	public boolean shouldBreakOnCaller(EObject instruction) {
+		final boolean res;
 
-    res = getBreakpointAttributes(instruction, IBreakpoint.ENABLED) == Boolean.TRUE;
+		res = getBreakpointAttributes(instruction, IBreakpoint.ENABLED) == Boolean.TRUE;
 
-    return res;
-  }
+		return res;
+	}
 }
